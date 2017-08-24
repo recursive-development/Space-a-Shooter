19bd7210901d621851835df0c12de2ede1535489
shako <timshako@gmail.com>
2017-08-24 19:14:13 +0300

    Добавление класса по работе с матрицами

--- shako/engine/src/com/shako/game/engine/math/Matrix.java
+++ shako/engine/src/com/shako/game/engine/math/Matrix.java
@@ -1,5 +1,8 @@
 package com.shako.game.engine.math;
 
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+
 /**
  * Класс по работе с матрицами.
  * - Перемещение.
@@ -12,9 +15,23 @@ package com.shako.game.engine.math;
 
 public class Matrix {
 
-
     /** */
     private Matrix() {
         // TO-DO
+    }
+
+    /** */
+    public static void transformation() {
+
+        //
+        Matrix3 matrix3x3 = new Matrix3();
+        // Приводим матрицу 3х3 к единичному ввиду
+        // 1 0 0
+        // 0 1 0
+        // 0 0 1
+        matrix3x3.idt();
+
+        //
+        Matrix4 matrix4x4 = new Matrix4();
     }
 }
