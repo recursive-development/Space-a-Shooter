753884839127503e344f135976237f715073e2c8
shako <timshako@gmail.com>
2017-08-25 00:34:09 +0300

    Добавил методы по работе с матрицами переходами

--- shako/engine/src/com/shako/game/engine/math/Matrix.java
+++ shako/engine/src/com/shako/game/engine/math/Matrix.java
@@ -68,15 +68,27 @@ public class Matrix {
 
     /** Расчёт матрицы перехода 3x3 */
     public static void calcTransitionMatrix(Matrix3 mat, Rect src, Rect dst) {
+
+        //
         float scaleX = dst.getWidth() / src.getWidth();
+
+        //
         float scaleY = dst.getHeight() / src.getHeight();
+
+        //
         mat.idt().translate(dst.pos.x, dst.pos.y).scale(scaleX, scaleY).translate(-src.pos.x, -src.pos.y);
     }
 
     /** Расчёт матрицы перехода 4x4 */
     public static void calcTransitionMatrix(Matrix4 mat, Rect src, Rect dst) {
+
+        //
         float scaleX = dst.getWidth() / src.getWidth();
+
+        //
         float scaleY = dst.getHeight() / src.getHeight();
+
+        //
         mat.idt().translate(dst.pos.x, dst.pos.y, 0f).scale(scaleX, scaleY, 1f).translate(-src.pos.x, -src.pos.y, 0f);
     }
 
