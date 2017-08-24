1a9051347ed0795b5663753424578420f99764b8
shako <timshako@gmail.com>
2017-08-24 12:45:54 +0300

    заприватил поля класса

--- shako/core/src/com/shako/spaceshooter/Main.java
+++ shako/core/src/com/shako/spaceshooter/Main.java
@@ -7,8 +7,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	/** */
+	private SpriteBatch batch;
+
+	/** */
+	private Texture img;
 	
 	@Override
 	public void create () {
