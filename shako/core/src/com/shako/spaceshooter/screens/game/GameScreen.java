e96875909f1dc05961c0ddbbc6d04481373cf8ac
shako <timshako@gmail.com>
2017-08-31 02:23:36 +0300

    Добавлен класс для игрового цикла

---
+++ shako/core/src/com/shako/spaceshooter/screens/game/GameScreen.java
@@ -0,0 +1,22 @@
+package com.shako.spaceshooter.screens.game;
+
+// --- libGDX ---
+import com.badlogic.gdx.Game;
+
+// --- Customs ---
+import com.shako.game.engine.Base2DScreen;
+
+/**
+ *
+ * @author Timur Kashapov
+ */
+
+public class GameScreen extends Base2DScreen {
+    /**
+     *
+     * @param game
+     */
+    public GameScreen(Game game) {
+        super(game);
+    }
+}
