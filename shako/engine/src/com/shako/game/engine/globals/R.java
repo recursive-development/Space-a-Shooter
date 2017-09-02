8800de864944a0879b41b29b5fc3cc54cbeb0622
Timur @shako Kashapov <timshako@gmail.com>
2017-09-02 18:45:02 +0300

    Ширина высота экрана
    Данные ширины и высоты экрана десктопного приложения по умолчанию

--- shako/engine/src/com/shako/game/engine/globals/R.java
+++ shako/engine/src/com/shako/game/engine/globals/R.java
@@ -1,8 +1,16 @@
 package com.shako.game.engine.globals;
 
 /**
+ * Глобальные данные для игрового приложения.
+ *
  * @author Timur Kashapov
  */
-
 public interface R {
+
+    /** */
+    int DEF_DESKTOP_SCREEN_WIDTH  = 640;
+    int DEF_DESKTOP_SCREEN_HEIGHT = 480;
+
+    /** */
+    String GAME_APP_TITLE = "Space-a-Shooter";
 }
