69c9194bedd11dfad385b44ba0a081354264b007
Timur @shako Kashapov <timshako@gmail.com>
2017-09-02 17:58:54 +0300

    Тестовая статистика игрового процесса

---
+++ shako/engine/src/com/shako/game/engine/stats/GameStats.java
@@ -0,0 +1,19 @@
+package com.shako.game.engine.stats;
+
+// --- libGDX ---
+import com.badlogic.gdx.Screen;
+
+/**
+ * Статистика игры.
+ *
+ * @author Timur Kashapov
+ */
+public abstract class GameStats extends Thread {
+
+    /** */
+    public GameStats(Screen screen) {
+
+        // TO-DO
+
+    } // GameStats()
+} // GameStats
