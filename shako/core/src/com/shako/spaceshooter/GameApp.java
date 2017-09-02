ab41510fdaf9fe5226883363a885ef90ec319585
Timur @shako Kashapov <timshako@gmail.com>
2017-09-02 17:59:18 +0300

    mini-refactor

--- shako/core/src/com/shako/spaceshooter/GameApp.java
+++ shako/core/src/com/shako/spaceshooter/GameApp.java
@@ -2,14 +2,13 @@ package com.shako.spaceshooter;
 
 // --- libGDX ---
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 
 // --- Customs ---
-import com.shako.spaceshooter.screens.menu.MenuScreen;
+// import com.shako.spaceshooter.screens.menu.MenuScreen;
 import com.shako.spaceshooter.screens.game.GameScreen;
 
 /**
- * Класс для запуска приложения.
+ * Класс запуска игрового приложения.
  *
  * @author Alexey Stepchenko
  * @author Timur Kashapov
@@ -17,39 +16,17 @@ import com.shako.spaceshooter.screens.game.GameScreen;
 public class GameApp extends Game {
 
     /** */
-    public static final float SCREEN_WIDTH = 960f;
-    public static final float SCREEN_HEIGHT = 1280f;
-
-    /** */
-    public static float VIEWPORT_LEFT;
-    public static float VIEWPORT_RIGHT;
-
-    /** */
     public GameApp() {
+    } // GameApp()
 
-    }
-
+    /** */
     @Override
     public void create() {
 
         // Устанавливаем начальный экран при запуске приложения.
-//        setScreen(new MenuScreen(this));
+        //
+        // setScreen(new MenuScreen(this));
         setScreen(new GameScreen(this));
-    }
 
-    @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
-
-        float aspectRatio = (float) width / height;
-        float viewportWidth = SCREEN_HEIGHT * aspectRatio;
-
-        VIEWPORT_LEFT = (SCREEN_WIDTH - viewportWidth) / 2;
-        VIEWPORT_RIGHT = VIEWPORT_LEFT + viewportWidth;
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-    }
-}
+    } // create()
+} // GameApp
