e2f3e7333bf55a626af5fb02833af5aff95ef303
Timur @shako Kashapov <timshako@gmail.com>
2017-09-02 18:45:58 +0300

    Изменил дефалтную ширину и высоту для экрана и title для приложения

--- shako/desktop/src/com/shako/spaceshooter/desktop/DesktopLauncher.java
+++ shako/desktop/src/com/shako/spaceshooter/desktop/DesktopLauncher.java
@@ -1,10 +1,12 @@
 package com.shako.spaceshooter.desktop;
 
-// --- ---
+// --- libGDX ---
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-// --- ----
+
+// --- Custom ----
 import com.shako.spaceshooter.GameApp;
+import com.shako.game.engine.globals.R;
 
 /** */
 public class DesktopLauncher {
@@ -14,6 +16,10 @@ public class DesktopLauncher {
 
         //
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title  = R.GAME_APP_TITLE;
+		config.width  = R.DEF_DESKTOP_SCREEN_WIDTH;
+		config.height = R.DEF_DESKTOP_SCREEN_HEIGHT;
 
         //
         new LwjglApplication(new GameApp(), config);
