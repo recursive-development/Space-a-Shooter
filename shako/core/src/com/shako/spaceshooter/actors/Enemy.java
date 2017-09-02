52bf1fde3f388c0e082faf349ac3adc1c11badde
Timur @shako Kashapov <timshako@gmail.com>
2017-09-02 17:58:14 +0300

    upd

--- shako/core/src/com/shako/spaceshooter/actors/Enemy.java
+++ shako/core/src/com/shako/spaceshooter/actors/Enemy.java
@@ -1,5 +1,8 @@
 package com.shako.spaceshooter.actors;
 
+// --- libGDX ---
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 /**
  *
  * @author Timur Kashapov
@@ -8,7 +11,7 @@ public class Enemy extends AActor {
 
     /** */
     public Enemy() {
-
+        super(new TextureRegion());
     } // Enemy()
 
 } // Enemy
